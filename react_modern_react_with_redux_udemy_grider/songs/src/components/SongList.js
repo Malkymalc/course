06b6e13cd6467e07bab091135dd5aa28e3d2c965
Malcolm Finlayson <malcolmfinlayson81@gmail.com>
2019-03-12 15:16:06 +0000

    Lessons 11/17 complete.

--- react_modern_react_with_redux_udemy_grider/songs/src/components/SongList.js
+++ react_modern_react_with_redux_udemy_grider/songs/src/components/SongList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions'
 
 class SongList extends Component {
 
   getSongItems = () => this.props.songs.map(song => {
+
+    const onClick = () => selectSong({song: song.title})
     return(
       <div className='item' key={song.title}>
         <div className='right floated content'>
-          <button className='ui button primary'>Select</button>
+          <button
+            className='ui button primary'
+            onClick={() => selectSong({})}
+          >Select</button>
         </div>
         <div className='content'>{song.title}</div>
       </div>
@@ -26,4 +32,4 @@ const mapStateToProps = (state) => {
   return {songs: state.songs};
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
