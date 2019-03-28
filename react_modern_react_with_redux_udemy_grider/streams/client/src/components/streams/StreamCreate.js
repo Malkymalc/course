3cd98f3e65194af4377cb3eba123dbc38160d245
Malcolm Finlayson <malcolmfinlayson81@gmail.com>
2019-03-28 19:11:26 +0000

    Some work on stream app. Working on auth atm.

---
+++ react_modern_react_with_redux_udemy_grider/streams/client/src/components/streams/StreamCreate.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const StreamCreate = () => {
+  return <div>StreamCreate</div>
+}
+
+export default StreamCreate;
---
+++ react_modern_react_with_redux_udemy_grider/streams/client/src/components/streams/StreamDelete.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const StreamDelete = () => {
+  return <div>StreamDelete</div>
+}
+
+export default StreamDelete;
